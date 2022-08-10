e24f7da169c50d15e4161e1c2093384eaffde0af
fabioalvesdeandrade29@gmail.com <fabioalvesdeandrade29@gmail.com>
2022-08-10 17:49:40 -0300

    página de user

--- src/App.tsx
+++ src/App.tsx
@@ -4,42 +4,87 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
-import CssBaseline from '@mui/material/CssBaseline';
+import { Container } from '@mui/material';
 
 const App = (props) => {
-	const [users, setUsers] = useState([
-		{ id: 1, name: "Nome 1" },
-    { id: 2, name: "Nome 2" },
-    { id: 3, name: "Nome 3" },
-    { id: 4, name: "Nome 4" },
-	]);
-  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		fetch("https://jsonplaceholder.typicode.com/users/")
-			.then((response) => response.json())
-			.then((json) => {setUsers(json); setLoading(false)});
-	});
-	return (  
-        <div className="App" style={{backgroundColor: 'purple'}}>
-			  <h1 style={{color: 'white', paddingTop: '100px', paddingLeft: '800px'}}>Lista de usuários</h1>
-			  <div className="card" style={{color: 'white', backgroundColor: 'purple', padding: '50px'}}>
-        {loading ? <h2>Carregando...</h2> : null}
-				<ul bstyle={{backgroundColor: 'purple'}}>
+	const [page,setPage] = useState(0);
+	const [mainUser,setMainUser] = useState(0);
+
+	const PageUsersList = () => {
+
+		const [users, setUsers] = useState([
+			{ id: 1, name: "Nome 1" },
+			{ id: 2, name: "Nome 2" },
+			{ id: 3, name: "Nome 3" },
+			{ id: 4, name: "Nome 4" },
+		]);
+		const [loading, setLoading] = useState(true);
+
+		useEffect(() => {
+			fetch("https://jsonplaceholder.typicode.com/users/")
+				.then((response) => response.json())
+				.then((json) => {setUsers(json); setLoading(false)});
+		});
+	
+		return (
+			<Container className="App">
+				<h2>Lista de usuários</h2>
+				<div className="card">
+				{loading ? <h2>Carregando...</h2> : null}
+				<ul>
 					<List>
-            {users.map((user) => (
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemText primary={user.name} />
-                </ListItemButton>
-          </ListItem>))}
-            </List>
-            <React.Fragment>
-          <CssBaseline />
-        </React.Fragment>
+						{users.map((user) => (
+							<ListItem disablePadding>
+								<ListItemButton onClick={() => {setPage(1); setMainUser(user.id)}}>
+									<ListItemText primary={user.name} />
+								</ListItemButton>
+					</ListItem>))}
+						</List>
+						<React.Fragment>
+				</React.Fragment>
 				</ul>
 			</div>
-		</div>
+		</Container>
+		)
+	}
+
+	const PageUser = () => {
+
+		const [loading, setLoading] = useState(true);
+
+		const [users, setUsers] = useState([
+			{ id: 1, name: "Nome 1" },
+			{ id: 2, name: "Nome 2" },
+			{ id: 3, name: "Nome 3" },
+			{ id: 4, name: "Nome 4" },
+		]);
+
+		var theUser = users.find(user => {return user.id === mainUser;})
+		if (theUser === undefined) {theUser = {id: 0, name: "noname"}}
+
+		useEffect(() => {
+			fetch("https://jsonplaceholder.typicode.com/users/")
+				.then((response) => response.json())
+				.then((json) => {setUsers(json); setLoading(false)});
+		});
+
+		return (
+			<h1>{theUser.name}</h1>
+		)
+	}
+
+	//O que você decide ver
+	const PageDisplay = (props : any) => {
+		if 			(page == 0) {return <PageUsersList/>;}
+		else if (page == 1) {return <PageUser/>;}
+
+		return <h1>ERRO 404</h1>;
+	}
+
+	//Oque você vê
+	return (  
+		<PageDisplay/>
 	);
 };
 
