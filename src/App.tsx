6062c61918f36846f7f0638bdf973b7e2fd9814e
ALEXSANDER ANULINO TEIXEIRA DA COSTA <alexsander.t@academico.ifrn.edu.br>
2022-08-11 15:32:43 -0300

    tarefas de usuarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
 import * as React from 'react';
+import Grid from '@mui/material/Grid';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
-import { Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
+import CheckIcon from '@mui/icons-material/Check';
+import ClearIcon from '@mui/icons-material/Clear';
 
-const App = (props) => {
+const App = (props:any) => {
 
 	const [page,setPage] = useState(0);
 	const [mainUser,setMainUser] = useState(0);
@@ -39,8 +42,8 @@ const App = (props) => {
 								<ListItemButton onClick={() => {setPage(1); setMainUser(user.id)}}>
 									<ListItemText primary={user.name} />
 								</ListItemButton>
-					</ListItem>))}
-						</List>
+						</ListItem>))}
+					</List>
 						<React.Fragment>
 				</React.Fragment>
 				</ul>
@@ -49,7 +52,7 @@ const App = (props) => {
 		)
 	}
 
-	const PageUser = () => {
+	const PageUserTasks = () => {
 
 		const [loading, setLoading] = useState(true);
 
@@ -58,6 +61,10 @@ const App = (props) => {
 			{ id: 2, name: "Nome 2" },
 			{ id: 3, name: "Nome 3" },
 			{ id: 4, name: "Nome 4" },
+		]);
+
+		const [tasks, setTasks] = useState([
+			{userId: 0, id: 0, title: "Carregando", completed: false},
 		]);
 
 		var theUser = users.find(user => {return user.id === mainUser;})
@@ -69,15 +76,31 @@ const App = (props) => {
 				.then((json) => {setUsers(json); setLoading(false)});
 		});
 
+		useEffect(() => {
+			fetch("https://jsonplaceholder.typicode.com/users/"+ (theUser!).id + "/todos")
+				.then((response) => response.json())
+				.then((json) => {setTasks(json); setLoading(false)});
+		});
+
 		return (
-			<h1>{theUser.name}</h1>
+			<Box>
+				<h1>{theUser.name}</h1>
+				<List>
+					{tasks.map((task) => (
+						<ListItem disablePadding>
+							<ListItemText primary={task.title} />
+							<CheckIcon />
+					</ListItem>))}
+				</List>
+			</Box>
+
 		)
 	}
 
 	//O que você decide ver
 	const PageDisplay = (props : any) => {
 		if 			(page == 0) {return <PageUsersList/>;}
-		else if (page == 1) {return <PageUser/>;}
+		else if (page == 1) {return <PageUserTasks/>;}
 
 		return <h1>ERRO 404</h1>;
 	}
